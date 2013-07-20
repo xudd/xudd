a7c2f3f57d43884c6f1bc7c6d1d0c92ca764dcfe
Christopher Allan Webber <cwebber@dustycloud.org>
2013-07-20 10:40:04 -0500

    XUDD setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, find_packages
+
+setup(
+    name="XUDD",
+    version="0.1.0-dev",
+    description="An actor model system for python",
+    author="Christopher Allan Webber",
+    author_email="cwebber@dustycloud.org",
+    url="https://github.com/cwebber/xudd",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+)
