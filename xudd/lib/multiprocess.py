9d73300a323c6f9ebc69886d7c34a38238760f5b
Christopher Allan Webber <cwebber@dustycloud.org>
2013-09-21 15:54:17 -0500

    spawn_multiprocess_hive code... no idea if this does anything yet :)

--- xudd/lib/multiprocess.py
+++ xudd/lib/multiprocess.py
@@ -5,8 +5,9 @@ from xudd.actor import Actor
 from xudd.tools import base64_uuid4
 
 
-def spawn_multiprocess_hive(hive_id):
-    pass
+def spawn_multiprocess_hive(hive_id, to_hive_queue, from_hive_queue):
+    hive = MultiProcessHive(hive_id, to_hive_queue, from_hive_queue)
+    hive.run()
 
 
 class MultiProcessAmbassador(Actor):
@@ -18,10 +19,12 @@ class MultiProcessAmbassador(Actor):
         self.remote_hive_id = base64_uuid4()
         self.to_hive_queue = Queue()
         self.from_hive_queue = Queue()
-        self.remote_hive = spawn_multiprocess_hive(
-            self.remote_hive_id,
-            self.to_hive_queue,
-            self.from_hive_queue)
+        self.multiproces_hive_proc = Process(
+            target=spawn_multiprocess_hive,
+            args=(
+                self.remote_hive_id,
+                self.to_hive_queue,
+                self.from_hive_queue))
 
         # Declare ourselves the ambassador for this hive
         self.send_message(
@@ -29,6 +32,7 @@ class MultiProcessAmbassador(Actor):
             directive="register_ambassador",
             body={
                 "hive_id": self.remote_hive_id})
+
 
 class MultiProcessHive(Hive):
     def __init__(self, hive_id, receive_queue, send_queue):
